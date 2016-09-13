9386c0581444e63e3a306732791856f9c9a0ca33
Joey <joey.lee@afusion.com>
2016-09-13 10:52:21 +0800

    Successfully loaded webview and created dialog box for input.

--- app/game/game.component.ts
+++ app/game/game.component.ts
@@ -1,8 +1,6 @@
 import {Component} from "@angular/core";
-// import webViewModule = require("ui/web-view");
-import frameModule = require('ui/frame');
-import pageModule = require('ui/page');
 import webViewModule = require("ui/web-view");
+import dialogs = require("ui/dialogs");
 
 @Component({
     selector: "game",
@@ -11,10 +9,10 @@ import webViewModule = require("ui/web-view");
 export class GameComponent {
     constructor() {};
 
+    private url = 'http://mobile3.gameassists.co.uk/MobileWebServices/casino/game/launch/12bet/ageOfDiscovery/en?casinoID=4107&lobbyURL=http://www.12bet.uk/en-gb/casino&bankingURL=&loginType=VanguardSessionToken&authToken=Flash_4760821572597542023&isRGI=true';
+
     ngOnInit() {
-        console.log('loading');
         var webView = new webViewModule.WebView();
-        console.dump('load');
         webView.on(webViewModule.WebView.loadFinishedEvent, function (args: webViewModule.LoadEventData) {
             // let message;
             console.log('inside');
@@ -27,6 +25,32 @@ export class GameComponent {
             // }
         });
         console.log(webViewModule.WebView.loadFinishedEvent);
-        webView.url = "http://www.example.com/";
+        webView.url = "https://www.google.com/";
+    }
+
+    tap() {
+        alert('test');
+    }
+
+    transfer() {
+        this.prompt('Transfer', 'Enter the amount that you wish to transfer.');
+    }
+
+    save() {
+        this.prompt('Save', 'Enter the amount that you wish to save.');
+    }
+
+    prompt(title, message) {
+        dialogs.prompt({
+            title: title,
+            message: message,
+            okButtonText: "Confirm",
+            cancelButtonText: "Cancel",
+            defaultText: "100",
+            inputType: dialogs.inputType.text
+        }).then(function (r) {
+            console.log("Response: " + r.result + ", Amount: " + r.text);
+            // if (dialogs.inputType.text)
+        });
     }
 }
